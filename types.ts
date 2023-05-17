cc4e587155b30ee0cfe865e3b113aaff1bfe579f
Вікторія Ільченко <v.ilchenko@kpi.ua>
2023-05-17 19:59:40 +0300

    Added types

---
+++ types.ts
@@ -0,0 +1,21 @@
+export type UsersResponseType = {
+    users: User[];
+    total: number;
+    skip:  number;
+    limit: number;
+}
+
+export type User = {
+    id: number;
+    firstName: string;
+    lastName: string;
+    address: Address;
+    image: string;
+}
+
+export type Address = {
+    address: string;
+    city: string;
+    postalCode: string;
+    state: string;
+}
