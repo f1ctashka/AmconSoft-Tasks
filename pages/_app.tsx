d54be78e1d7a9d1c10f6dc8cde4679f68b874327
Вікторія Ільченко <v.ilchenko@kpi.ua>
2023-05-17 21:24:21 +0300

    Added  verification on the correct path

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Custom404 from '@/pages/404';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps, router }: AppProps) => {
+  const { asPath } = router;
+
+  const is404Page = asPath === '/404';
+  if (is404Page) {
+    return <Custom404 />;
+  }
+
+  return <Component {...pageProps} />;
 }
+
+export default App;
